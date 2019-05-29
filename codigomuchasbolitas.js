84f69f19afcacb11bc6b2b65b16271d4d081e7c5
Yuniquamiranda <51093732+Yuniquamiranda@users.noreply.github.com>
2019-05-29 11:17:42 -0500

    Add files via upload

---
+++ codigomuchasbolitas.js
@@ -0,0 +1,24 @@
+var ball;
+ball = create();
+size (ball,200,200);
+color(ball,255,0,0);
+move (ball,50,50)
+var counter= 0;
+while (counter<10) {
+   create();
+  counter++;
+}
+//var list= [];
+//list.push(create());
+//list.push(create());
+//list.push(create());
+//list.push(create());
+
+//var i=0;
+//var length= list.lenght; 
+//console.log(lenght)
+ //while (i < lenght){
+    // var ball = ball[i];
+    // colorRandom(ball);
+    // i++;
+ //}
